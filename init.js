d9603d2a9656a78a769ea588340b42375194cb24
nhz-io <nhz-io@users.noreply.github.com>
2014-07-28 04:05:42 +0300

    quick and dirty init.js instead of init.sh

---
+++ init.js
@@ -0,0 +1,165 @@
+var Git = require('git-wrapper');
+var fs = require('fs');
+var npm = require('npm');
+var git = new Git;
+
+var npm_install_args = [
+  "gulp", "gulp-coffee", "gulp-cache", "gulp-coffeelint", "gulp-util",
+  "gulp-mocha", "gulp-rimraf", "run-sequence", "coffee-script", "coffeelint",
+  "coffeelint-stylish", "should", "gulp-exec", "gulp-rename", "gulp-uglify",
+  "gulp-js-prettify", "gulp-git", "gulp-json-editor", "gulp-jade", "gulp-stylus",
+  "gulp-watch"
+];
+
+var git_master, git_dev;
+
+git_master = function() {
+  git.exec('checkout dev', function(err, msg) {
+    if (err) throw(err);
+    console.log(msg);
+
+    git.exec('checkout master', function(err, msg) {
+      if (err) throw(err);
+      console.log(msg);
+
+      git.exec('rm ./init.js', function(err, msg) {
+        if (err) throw(err);
+        console.log(msg);
+
+        git.exec('rm README.md', function(err, msg) {
+          if (!err && msg) console.log(msg);
+
+          git.exec('rm package.json', function(err, msg) {
+            if (!err && msg) console.log(msg);
+
+            git.exec('update-ref -d refs/heads/master', function(err, msg) {
+              git.exec('add .', function(err, msg) {
+                if (err) throw(err);
+                console.log(msg);
+
+                git.exec('commit -m "Scaffold Initialized"', function(err, msg) {
+                  if (err) throw(err);
+                  console.log(msg);
+
+                  git.exec('checkout --orphan clean', function(err, msg) {
+                    if (err) throw(err);
+                    console.log(msg);
+
+                    git.exec('rm . -rf', function(err, msg) {
+                      if (err) throw(err);
+                      console.log(msg);
+
+                      git.exec('update-ref -d refs/heads/clean', function(err, msg) {
+                        if (err) throw(err);
+                        console.log(msg);
+
+                        fs.closeSync(fs.openSync('./README.md', 'w'));
+
+                        git.exec('add ./README.md', function(err, msg) {
+                          if (err) throw(err);
+                          console.log(msg);
+
+                          npm.commands.init(function(err) {
+                            if (err) throw(err);
+
+                            git.exec('add ./package.json', function(err, msg) {
+                              if(err) throw(err);
+                              console.log(msg);
+
+                              git.exec('commit -m "Clean master"', function(err, msg) {
+                                if (err) throw(err);
+                                console.log(msg);
+
+                                git.exec('checkout master', function(err, msg) {
+                                  if (err) throw(err);
+                                  console.log(msg);
+
+                                  git.exec('rebase clean', function(err, msg) {
+                                    if (err) throw(err);
+                                    console.log(msg);
+
+                                        git_dev();
+                                  });
+                                });
+                              });
+                            });
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+git_dev = function() {
+  git.exec('checkout dev', function(err, msg) {
+    if (err) throw(err);
+    console.log(msg);
+
+    git.exec('rm README.md', function(err, msg) {
+      if (!err && msg) console.log(msg);
+
+      git.exec('rm package.json', function(err, msg) {
+        if(err) throw(err);
+        console.log(msg);
+
+        git.exec('update-ref -d refs/heads/dev', function(err, msg) {
+          if (err) throw(err);
+          console.log(msg);
+
+          git.exec('add --all .', function(err, msg) {
+            if (err) throw(err);
+            console.log(msg);
+
+            git.exec('commit -m "Scaffold Initialized"', function(err, msg) {
+              if (err) throw(err);
+              console.log(msg);
+
+              git.exec('rebase clean', function(err, msg) {
+                if (err) throw(err);
+                console.log(msg);
+
+                npm.commands.install(npm_install_args, function(err) {
+                  if (err) throw(err);
+
+                  git.exec('add ./package.json', function(err, msg) {
+                    if (err) throw(err);
+                    console.log(msg);
+
+                    git.exec('commit --amend --no-edit', function(err, msg) {
+                      if (err) throw(err);
+                      console.log(msg);
+
+                      git.exec('remote rm origin', function(err, msg) {
+                        if (!err && msg) console.log(msg);
+
+                        git.exec('branch -D clean', function(err, msg) {
+                          if (err) throw(err);
+                          console.log(msg);
+
+                          console.log("DONE!");
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+}
+
+npm.load({"save-dev":true}, function(err) {
+  if (err) throw(err);
+  git_master();
+});
